796cc7cd24eef74aec45546e8782e3b563fe8603
tota113 <105284820+tota113@users.noreply.github.com>
2022-10-26 08:51:41 -0300

    Update BAGULHO JAO.sql

--- BAGULHO JAO.sql
+++ BAGULHO JAO.sql
@@ -31,4 +31,27 @@ insert into Diciplinas
 (COD_DIC,nome_DIC,carga_hor)
 values
 ('ENG','ENGENHARIA DE SOFTWARE',80),
+alter table Alunos
+modify column MAT int not null PRIMARY KEY
 
+alter table Diciplinas
+modify column COD_DIC int not null PRIMARY KEY
+
+alter table Professores
+modify column COD_PROF int not null PRIMARY KEY
+
+
+create table Turma
+(
+COD_TURMA int not null Primary Key,
+COD_DIC int not null,
+COD_PROF int not null,
+ANO varchar(32) not null,
+horario varchar(12) not null
+)
+
+alter table Turma
+Add Foreign Key (COD_DIC) references Diciplinas(COD_DIC);
+
+alter table Turma
+Add Foreign Key (COD_PROF) references Professores(COD_PROF);
